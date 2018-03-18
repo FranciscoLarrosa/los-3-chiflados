0f10026e38ceab6de8036386de97733f02e73d10
juan <juany93@gmail.com>
2018-03-18 17:27:00 -0300

    que ranciedad

--- test-animales/src/test/animales/ComparadorPeso.java
+++ test-animales/src/test/animales/ComparadorPeso.java
@@ -4,14 +4,14 @@ package test.animales;
 
 public class ComparadorPeso {
     public void verificarPeso(Animal a,Animal b){
-        if (a.comapareTo(b)== -1) {
+        if (a.comapareTo(b)== 1) {
             System.out.println("Elprimer animal es mas pesado q el segundo");
             
         }
         if (a.comapareTo(b)== 0) {
             System.out.println("Pesan lo mismo");
         }
-        if (a.comapareTo(b)== 1) {
+        if (a.comapareTo(b)== -1) {
             System.out.println("el segundo es mas pesado");
         }
     }
--- test-animales/src/test/animales/TestComparadorPeso.java
+++ test-animales/src/test/animales/TestComparadorPeso.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package test.animales;
-
+import java.util.Scanner;
 /**
  *
  * @author Satoru
@@ -12,10 +12,13 @@ package test.animales;
 public class TestComparadorPeso {
     //EJERCICIO PARTE 7:
     public static void main(String[] args) {
+        
+        Scanner asd = new Scanner(System.in);
+        
         Gato a = new Gato();
         Gato b = new Gato();
-        a.setPeso(100);
-        b.setPeso(90);
+        a.setPeso(asd.nextInt());
+        b.setPeso(asd.nextInt());
         ComparadorPeso d = new ComparadorPeso();
         d.verificarPeso(a, b);
     }
